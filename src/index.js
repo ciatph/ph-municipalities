c6614b3c865d8bcc468b442ce9a96c40632042b5
ciatph <ciat.ph@gmail.com>
2022-08-08 04:59:14 +0800

    feat: List the municipalities of given provinces, #1

--- src/index.js
+++ src/index.js
@@ -9,7 +9,29 @@ const main = async () => {
   })
 
   try {
+    // Load formatted data from the excel file
     await file.init()
+  } catch (err) {
+    console.log(`[ERROR]: ${err.message}`)
+  }
+
+  try {
+    // List hand-picked province municipalities
+    const provinces = file.listMunicipalities({
+      provinces: ['Albay', 'Camarines Norte', 'Camarines Sur', 'Catanduanes', 'Masbate', 'Sorsogon']
+    })
+
+    // Write logs to console
+    let count = 0
+    let stats = '\nPROVINCES:\n'
+
+    for (const province in provinces) {
+      count += provinces[province].length
+      stats += `${province}: ${provinces[province].length}, ${provinces[province].toString()}\n`
+    }
+
+    console.log(stats)
+    console.log(`total: ${count}`)
   } catch (err) {
     console.log(`[ERROR]: ${err.message}`)
   }
