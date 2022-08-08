0844636da25eea31d3898b912f661a0f85d824b6
ciatph <ciat.ph@gmail.com>
2022-08-08 16:46:56 +0800

    fix: linebreak-style errors on release. Also, update the ymls directly from the dev branch once (gh actions quirks).

---
+++ .eslintrc_win.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: [
+    'standard',
+    'eslint:recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2022
+  },
+  rules: {
+    indent: ['error', 2],
+    // 'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    // 'no-console': ['error', { 'allow': ['error'] }]
+    // 'no-unused-vars': 'off',
+    // 'no-undef': 'off'
+  }
+}
