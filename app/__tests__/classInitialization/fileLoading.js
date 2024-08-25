4cb2b616459313572c8bf48e9dc6cdf0e3820836
ciatph <ciat.ph@gmail.com>
2024-08-26 02:19:24 +0800

    chore: use common tests

--- app/__tests__/classInitialization/fileLoading.js
+++ app/__tests__/classInitialization/fileLoading.js
@@ -6,37 +6,45 @@ const ExcelFactory = require('../../src/classes/excelfactory')
 
 const config = require('./config.json')
 
+/* eslint-disable no-undef */
+const test = (excelFile, excelFactory, isRemote = false) => {
+  expect(excelFactory).toBeDefined()
+  expect(excelFactory instanceof ExcelFactory).toBe(true)
+  expect(Array.isArray(excelFactory.datalist)).toBe(true)
+  expect(excelFactory.datalist.length).toBeGreaterThan(0)
+
+  expect(excelFile).toBeDefined()
+  expect(excelFile instanceof ExcelFile).toBe(true)
+  expect(Array.isArray(excelFile.datalist)).toBe(true)
+  expect(excelFile.datalist.length).toBeGreaterThan(0)
+
+  if (isRemote) {
+    expect(typeof excelFactory.url).toBe('string')
+    expect(typeof excelFile.url).toBe('string')
+  } else {
+    expect(excelFactory.url).toBeNull()
+    expect(excelFile.url).toBeNull()
+  }
+}
+
 describe('Class intialization using DEFAULT config', () => {
   jest.setTimeout(30000)
 
   it('should load local Excel file', () => {
-    // ExcelFile
     const excelFile = new ExcelFile({
       pathToFile: path.join(__dirname, '..', '..', 'data', 'day1.xlsx')
     })
 
-    // ExcelFactory
     const excelFactory = new ExcelFactory()
-
-    expect(excelFactory).toBeDefined()
-    expect(excelFactory instanceof ExcelFactory).toBe(true)
-    expect(Array.isArray(excelFactory.datalist)).toBe(true)
-    expect(excelFactory.datalist.length).toBeGreaterThan(0)
-
-    expect(excelFile).toBeDefined()
-    expect(excelFile instanceof ExcelFile).toBe(true)
-    expect(Array.isArray(excelFile.datalist)).toBe(true)
-    expect(excelFile.datalist.length).toBeGreaterThan(0)
+    test(excelFile, excelFactory)
   })
 
   it('should load remote Excel file', async () => {
-    // ExcelFile
     const excelFile = new ExcelFile({
       pathToFile: path.join(__dirname, 'excelfiledownload.xlsx'),
       url: process.env.EXCEL_FILE_URL
     })
 
-    // ExcelFactory
     const excelFactory = new ExcelFactory({
       url: process.env.EXCEL_FILE_URL
     })
@@ -45,15 +53,7 @@ describe('Class intialization using DEFAULT config', () => {
     await excelFile.init()
     await excelFactory.init()
 
-    expect(excelFactory).toBeDefined()
-    expect(excelFactory instanceof ExcelFactory).toBe(true)
-    expect(Array.isArray(excelFactory.datalist)).toBe(true)
-    expect(excelFactory.datalist.length).toBeGreaterThan(0)
-
-    expect(excelFile).toBeDefined()
-    expect(excelFile instanceof ExcelFile).toBe(true)
-    expect(Array.isArray(excelFile.datalist)).toBe(true)
-    expect(excelFile.datalist.length).toBeGreaterThan(0)
+    test(excelFile, excelFactory, true)
   })
 })
 
@@ -61,39 +61,25 @@ describe('Class intialization using CUSTOM config', () => {
   jest.setTimeout(30000)
 
   it('should load local Excel file', () => {
-    // ExcelFile
     const excelFile = new ExcelFile({
       pathToFile: path.join(__dirname, '..', '..', 'data', 'day1.xlsx'),
       settings: config
     })
 
-    // ExcelFactory
     const excelFactory = new ExcelFactory({
       settings: config
     })
 
-    expect(excelFactory).toBeDefined()
-    expect(excelFactory instanceof ExcelFactory).toBe(true)
-    expect(excelFactory.url).toBeNull()
-    expect(Array.isArray(excelFactory.datalist)).toBe(true)
-    expect(excelFactory.datalist.length).toBeGreaterThan(0)
-
-    expect(excelFile).toBeDefined()
-    expect(excelFile instanceof ExcelFile).toBe(true)
-    expect(excelFile.url).toBeNull()
-    expect(Array.isArray(excelFile.datalist)).toBe(true)
-    expect(excelFile.datalist.length).toBeGreaterThan(0)
+    test(excelFile, excelFactory)
   })
 
   it('should load remote Excel file', async () => {
-    // ExcelFile
     const excelFile = new ExcelFile({
       pathToFile: path.join(__dirname, 'excelfiledownload.xlsx'),
       url: process.env.EXCEL_FILE_URL,
       settings: config
     })
 
-    // ExcelFactory
     const excelFactory = new ExcelFactory({
       url: process.env.EXCEL_FILE_URL,
       settings: config
@@ -103,16 +89,6 @@ describe('Class intialization using CUSTOM config', () => {
     await excelFile.init()
     await excelFactory.init()
 
-    expect(excelFactory).toBeDefined()
-    expect(excelFactory instanceof ExcelFactory).toBe(true)
-    expect(typeof excelFactory.url).toBe('string')
-    expect(Array.isArray(excelFactory.datalist)).toBe(true)
-    expect(excelFactory.datalist.length).toBeGreaterThan(0)
-
-    expect(excelFile).toBeDefined()
-    expect(excelFile instanceof ExcelFile).toBe(true)
-    expect(typeof excelFile.url).toBe('string')
-    expect(Array.isArray(excelFile.datalist)).toBe(true)
-    expect(excelFile.datalist.length).toBeGreaterThan(0)
+    test(excelFile, excelFactory, true)
   })
 })
