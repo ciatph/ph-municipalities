eb75da79263919ad98a019dc1cb9e40306f14699
ciatph <ciat.ph@gmail.com>
2024-08-26 23:12:31 +0800

    chore: refactor the municipalities test script, #68

--- app/__tests__/municipalities/municipalitiesCount.js
+++ app/__tests__/municipalities/municipalitiesCount.js
@@ -6,6 +6,7 @@ const ColorLog = require('../../src/classes/colorlog')
 const logger = new ColorLog({ color: ColorLog.COLORS.TEXT.YELLOW, isBold: true })
 
 const checkClass = require('../classInitialization/checkClass')
+const createMunicipalityInstance = require('./createMunicipalityInstance')
 const { arrayToString } = require('../../src/lib/utils')
 
 /* eslint-disable no-undef */
@@ -28,32 +29,16 @@ describe('Municipalities total count match', () => {
       classType: ExcelFile
     })
 
-    // Unique provinces/municipalities from the 10-day Excel file
-    const allExcelProvinces = excelFile.datalist
-      .map(item => item.province)
-      .filter((x, i, a) => a.indexOf(x) === i)
-    const excelMunicipalities = excelFile.listMunicipalities({ provinces: allExcelProvinces })
-    const countExcelMunicipalities = Object.values(excelMunicipalities).reduce((sum, item) => sum + item.length, 0)
-
-    // Unique provinces/municipalities from the config file (PAGASA Seasonal)
-    const allProvinces = new Set(excelFile.listRegions('provinces').flat())
-    const configMunicipalities = excelFile.listMunicipalities({ provinces: [...allProvinces] })
-    const countConfigMunicipalities = Object.values(configMunicipalities).reduce((sum, item) => sum + item.length, 0)
-
-    // Municipalities exist in 10-day Excel but not in the municipalities list built from the PAGASA seasonal config
-    const hasMissingInConfig = (countExcelMunicipalities - countConfigMunicipalities) > 0
-    // Province(s) exist in the PAGASA seasonal config but not in the 10-day Excel
-    const hasMissingInExcel = (allProvinces.size - allExcelProvinces.length) > 0
-
-    logger.log(
-      `[INFO]: Parsed municipalities from config: ${countConfigMunicipalities}\n` +
-      `loaded municipalities from Excel file: ${countExcelMunicipalities}\n`, {
-        color: ColorLog.COLORS.TEXT.CYAN
-      })
+    const {
+      excel,
+      config,
+      hasMissingInExcel,
+      hasMissingInConfig
+    } = createMunicipalityInstance(excelFile)
 
     // Process missing PAGASA seasonal config provinces/municipalities
     if (hasMissingInConfig) {
-      const fromConfig = allExcelProvinces.filter(item => !allProvinces.has(item))
+      const fromConfig = excel.provinces.filter(item => !config.provinces.has(item))
 
       const countMissingConfig = Object.values(
         excelFile.listMunicipalities({ provinces: fromConfig })
@@ -68,7 +53,7 @@ describe('Municipalities total count match', () => {
 
     // Process missing 10-day Excel provinces/municipalities
     if (hasMissingInExcel) {
-      const fromExcel = [...allProvinces].filter(item => !allExcelProvinces.includes(item))
+      const fromExcel = [...config.provinces].filter(item => !excel.provinces.includes(item))
 
       logger.log(
         `[WARNING]: ${fromExcel.length} PROVINCE(S) PRESENT in the PAGASA seasonal config\n` +
@@ -79,7 +64,7 @@ describe('Municipalities total count match', () => {
 
     if (hasMissingInConfig || hasMissingInExcel) {
       logger.log(
-        '[INFO]: If you believe these RED warning(s) are incorrect, feel free to create an Issue\n' +
+        '[INFO]: If you believe these RED warning(s) are incorrect, feel free to reach out\n' +
         'or extend and override the ExcelFile or ExcelFactory classes in your scripts.', {
           color: ColorLog.COLORS.TEXT.RED
         })
@@ -89,9 +74,10 @@ describe('Municipalities total count match', () => {
       })
     }
 
-    let passMsg = '[20240826]: Allow test to succeed here since there is little information about updated\n'
-    passMsg += 'PAGASA seasonal & 10-day province/municipalities naming conventions for the other regions.\n'
-    passMsg += 'Take note of the INFOS/WARNINGS and extend/override the class methods to accommodate custom settings as neccessary'
+    let passMsg = '[20240826]: Allow the test to succeed here since there is little information about updated\n'
+    passMsg += 'PAGASA seasonal & 10-day province/municipalities naming conventions for the other regions, and they\n'
+    passMsg += 'may change anytime without prior notice. Take note of the INFOS/WARNINGS and\n'
+    passMsg += 'extend/override the class methods on custom scripts to accommodate custom settings as necessary'
     expect(logger.log(passMsg, { color: ColorLog.COLORS.TEXT.YELLOW })).toBe(undefined)
 
     /* Uncomment true "tests" for testing
