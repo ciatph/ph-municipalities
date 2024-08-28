af3c7842e7c5788a96e31f1c3162af92aa7d61e0
ciatph <ciat.ph@gmail.com>
2024-08-28 23:50:34 +0800

    wip: test custom and local paghasa regions settings on separate files

---
+++ app/__tests__/classInitialization/defaultSettings.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+const path = require('path')
+
+const ExcelFile = require('../../src/classes/excel')
+const ExcelFactory = require('../../src/classes/excelfactory')
+const ColorLog = require('../../src/classes/colorlog')
+const logger = new ColorLog({ isBold: true })
+
+const checkClass = require('./checkClass')
+
+// Classes loading the default local 10-day Excel file
+const local = {
+  excelFactory: new ExcelFactory(),
+
+  excelFile: new ExcelFile({
+    pathToFile: path.join(__dirname, '..', '..', 'data', 'day1.xlsx')
+  })
+}
+
+// Classes loading the remote 10-day Excel file
+const remote = {
+  excelFile: new ExcelFile({
+    pathToFile: path.join(__dirname, 'excelfiledownload4.xlsx'),
+    url: process.env.EXCEL_FILE_URL
+  }),
+
+  excelFactory: new ExcelFactory({
+    url: process.env.EXCEL_FILE_URL
+  })
+}
+
+/* eslint-disable no-undef */
+describe('Class intialization using DEFAULT config', () => {
+  beforeAll(async () => {
+    return await Promise.all([
+      remote.excelFile.init(),
+      remote.excelFactory.init()
+    ])
+  })
+
+  it('should load local Excel file', () => {
+    checkClass({
+      excelInstance: local.excelFactory,
+      classType: ExcelFactory
+    })
+
+    checkClass({
+      excelInstance: local.excelFile,
+      classType: ExcelFile
+    })
+  })
+
+  it('should load remote Excel file', async () => {
+    jest.setTimeout(20000)
+    logger.log('[INIT]: Started loading using "DEFAULT" config')
+
+    checkClass({
+      excelInstance: remote.excelFactory,
+      isRemote: true,
+      classType: ExcelFactory
+    })
+
+    checkClass({
+      excelInstance: remote.excelFile,
+      isRemote: true,
+      classType: ExcelFile
+    })
+  })
+})
