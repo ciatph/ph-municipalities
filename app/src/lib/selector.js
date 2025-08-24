05bf991856567d2bc50018e778848d451e98f750
ciatph <ciat.ph@gmail.com>
2025-08-24 23:32:12 +0800

    chore: use env variable for default archived excel url, #156

--- app/src/lib/selector.js
+++ app/src/lib/selector.js
@@ -15,23 +15,25 @@ const selectDataSource = async () => {
   let exit = false
   let url
   let ExcelHandler = null
+
   const logger = new ColorLog({ color: ColorLog.COLORS.TEXT.YELLOW, isBold: true })
 
   // Warning messages
+  const archivedUrl = process.env.ARCHIVED_EXCEL_FILE_URL ||
+    'https://raw.githubusercontent.com/ciatph/ph-municipalities/refs/heads/archives/day1.xlsx'
+
+  let msgRemote = `[⚠️  WARNING]: Should you still want to use a remote Excel file, you can use the archived remote Excel file as data source at:\n${archivedUrl}\n`
+  const msgUseDefault = '[⚠️  WARNING]: Please use the default local Excel file as data source.\n'
   let msgWarn = '[⚠️  WARNING]: PAGASA 10-Day Excel files are no longer available.\n'
   msgWarn += 'https://github.com/ciatph/ph-municipalities/issues/156\n'
-
-  let msgRemote = '[⚠️  WARNING]: Should you still want to use a remote Excel file, you can use the archived remote Excel file as data source at:\n'
-  msgRemote += 'https://raw.githubusercontent.com/ciatph/ph-municipalities/refs/heads/archives/day1.xlsx'
-
-  const msgUseDefault = '[⚠️  WARNING]: Please use the default local Excel file as data source.\n'
 
   while (!exit) {
     // Prompt to enter the download URL of a remote excel file
     if (url === undefined) {
       const askDownload = await prompt('\nWould you like to download and use a remote PAGASA 10-Day Excel file?\nPress enter to ignore. Press Y and enter to proceed. [n/Y]: ')
+      const askDownloadValue = String(askDownload).trim().toUpperCase()
 
-      if (askDownload === 'Y') {
+      if (askDownloadValue === 'Y') {
         while (!url) {
           logger.log(msgWarn, { color: ColorLog.COLORS.TEXT.RED })
           logger.log(msgUseDefault, { color: ColorLog.COLORS.TEXT.GREEN })
